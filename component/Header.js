35d30620fbc376ed9d2abd43bbfb26ea296938d4
aditya170204 <adityaraj17feb@gmail.com>
2024-11-30 18:28:15 +0530

    first commit

---
+++ component/Header.js
@@ -0,0 +1,12 @@
+import {StyleSheet, Text, View} from 'react-native';
+import React from 'react';
+
+export default function Header() {
+  return (
+    <View>
+      <Text style={{fontSize: 20}}>Header</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({});
